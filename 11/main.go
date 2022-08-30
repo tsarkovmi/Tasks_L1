8c8a26502ea1195248f86a92c4496099625e47bf
Tsarkov Michael <tsarkovmichael@gmail.com>
2022-08-30 09:50:28 +0300

    comments added

--- 11/main.go
+++ 11/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func setIntersetcion[T constraints.Ordered](arr1 []T, arr2 []T) []T {
-	m := make(map[T]int)
+	m := make(map[T]int) //создадим мапу с ключём передаваемого типа
 
 	for _, v := range arr1 {
 		m[v] += 1
@@ -16,20 +16,19 @@ func setIntersetcion[T constraints.Ordered](arr1 []T, arr2 []T) []T {
 		m[v] += 1
 	}
 
-	res := make([]T, 0)
-	for k, v := range m {
-		if v > 1 {
-			res = append(res, k)
+	res := make([]T, 0)   //создали пустой срез необходимого типа
+	for k, v := range m { //цикл пока есть ключи в мапе
+		if v > 1 { //находим пересечение двух множеств
+			res = append(res, k) //если оно есть, добавляем в массив число, которое пересекается
 		}
 	}
 
-	return res
+	return res //возвращаем массив
 }
 
 func main() {
-	set1 := []int{1, 2, 3, 4, 5, 10}
-	set2 := []int{6, 4, 3, 8}
-	fmt.Printf(" first set: %v\nsecond set: %v\n", set1, set2)
+	set1 := []int{1, 2, 3, 4, 5, 10} //первое множетсво, представленно как срез интов
+	set2 := []int{6, 4, 3, 8}        //второе множество
 
 	res := setIntersetcion(set1, set2)
 	fmt.Printf("intersection: %v\n", res)
--- 12/main.go
+++ 12/main.go
@@ -3,18 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	arr := []string{"cat", "cat", "dog", "cat", "tree"}
-	fmt.Printf("set: %v\n", arr)
+	arr := []string{"cat", "cat", "dog", "cat", "tree"} //инициализировали необходимый массив
 
-	m := make(map[string]struct{})
-	res := make([]string, 0)
+	m := make(map[string]struct{}) //создали пустую мапу
+	res := make([]string, 0)       //создали срез строк
 
-	for _, v := range arr {
-		if _, ok := m[v]; ok {
+	for _, v := range arr { //идём по срезу строк, читаем в v, значения
+		if _, ok := m[v]; ok { //если в мапе, по данному ключу уже есть данные, то пропускаем
 			continue
 		}
-		res = append(res, v)
-		m[v] = struct{}{}
+		res = append(res, v) //если нет, то делаем запись в срез строк
+		m[v] = struct{}{}    //делаем метку, что ключ добавлен
 	}
 
 	fmt.Printf("subset: %v\n", res)
